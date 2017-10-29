755b391c0ecd7495c7a79a7c15db9f8d75df9bdc
Juan Pablo Orsay <juan.orsay@avature.net>
2017-10-29 19:13:44 -0300

    Add missing default scripts

---
+++ ej2.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./venv/bin/python ./src/ex2.py --source tsc1.fasta --blast remote --dest ej2_out.txt
---
+++ ej3.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./venv/bin/python ./src/ex3.py --source ./aln-fasta.fasta
